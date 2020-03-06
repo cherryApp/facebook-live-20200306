a4bc995949ac5912e9cdb7403c88709a5f13bc68
cherryApp <j.cserko@gmail.com>
2020-03-06 11:28:59 +0100

    Feat: index page.

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-app.get('/', (req, res) => res.send('hello from express'));
+// Template engine.
+app.set('views', './views');
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+
+app.use('/', require('./routes/index'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
