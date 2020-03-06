28a5f3fb75620dd1b4f75545327efb15259ff678
cherryApp <j.cserko@gmail.com>
2020-03-06 12:11:00 +0100

    Feat: sass done.

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3001;
 
 // Template engine.
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views') );
 app.set('view engine', 'pug');
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use('/', require('./routes/index'));
 
