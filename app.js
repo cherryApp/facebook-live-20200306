6181be751d2916259eeee7c5b29e49bbac04330f
CherryApp <j.cserko@gmail.com>
2020-03-06 11:37:48 +0000

    fix port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3001;
+const port = 80;
 
 // Template engine.
 app.set('views', path.join(__dirname, 'views') );
